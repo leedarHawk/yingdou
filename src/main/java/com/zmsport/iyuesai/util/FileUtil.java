d2e640130d4227da326b41239601af3caa8a460d
hawk <hawk.wan@139.com>
2015-09-21 15:38:40 +0800

    add all

--- src/main/java/com/zmsport/iyuesai/util/FileUtil.java
+++ src/main/java/com/zmsport/iyuesai/util/FileUtil.java
@@ -1,10 +1,7 @@
 package com.zmsport.iyuesai.util;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 import javax.imageio.ImageIO;
@@ -15,202 +12,205 @@ import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 文件通用类
- * 
- * @author bilei
  *
+ * @author bilei
  */
 public final class FileUtil {
-	
-	private static Logger log = LoggerFactory.getLogger(FileUtil.class);
 
-	/**
-	 * 上传图片目录
-	 */
-	public static String UPLOAD_DIR = "upload";
+    private static Logger log = LoggerFactory.getLogger(FileUtil.class);
 
-	/**
-	 * 比赛图片目录
-	 */
-	public static String GAME_DIR = "game";
+    /**
+     * 上传图片目录
+     */
+    public static String UPLOAD_DIR = "upload";
 
-	/**
-	 * 球队图片目录
-	 */
-	public static String TEAM_DIR = "team";
-	
-	static {
-		Properties p = new Properties();  
-        try {  
+    /**
+     * 比赛图片目录
+     */
+    public static String GAME_DIR = "game";
+
+    /**
+     * 球队图片目录
+     */
+    public static String TEAM_DIR = "team";
+
+    static {
+        Properties p = new Properties();
+        try {
             InputStream in = FileUtil.class.getResourceAsStream("/config.properties");
-            p.load(in);  
-            in.close();  
-            if(p.getProperty("upload.dir") != null && p.getProperty("upload.dir").length() > 0) {
-            	UPLOAD_DIR = p.getProperty("upload.dir");
+            p.load(in);
+            in.close();
+            if (p.getProperty("upload.dir") != null && p.getProperty("upload.dir").length() > 0) {
+                UPLOAD_DIR = p.getProperty("upload.dir");
             }
-            if(p.getProperty("game.dir") != null && p.getProperty("game.dir").length() > 0) {
-            	GAME_DIR = p.getProperty("game.dir");
+            if (p.getProperty("game.dir") != null && p.getProperty("game.dir").length() > 0) {
+                GAME_DIR = p.getProperty("game.dir");
             }
-            if(p.getProperty("team.dir") != null && p.getProperty("team.dir").length() > 0) {
-            	TEAM_DIR = p.getProperty("team.dir");
+            if (p.getProperty("team.dir") != null && p.getProperty("team.dir").length() > 0) {
+                TEAM_DIR = p.getProperty("team.dir");
             }
-        } catch (IOException ex) {  
-        	log.error("读取config.properties文件错误");
-        }  
-	}
+        } catch (IOException ex) {
+            log.error("读取config.properties文件错误");
+        }
+    }
 
-	/**
-	 * 上传比赛图片文件
-	 * 
-	 * @param file
-	 * @param realPath
-	 * @param rootPath
-	 * @return
-	 */
-	@SuppressWarnings("finally")
-	public static String uploadGamePic(MultipartFile file, String realPath) {
-		String fileName = "game_" + System.currentTimeMillis() + "."
-				+ file.getOriginalFilename().split("\\.")[1];
-		String absolutePath = realPath + File.separator + UPLOAD_DIR
-				+ File.separator + GAME_DIR;
-		String relativePath = File.separator + UPLOAD_DIR + File.separator
-				+ GAME_DIR;
-		File targetFile = new File(absolutePath, fileName);
-		File dir = new File(absolutePath);
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
-		try {
-			file.transferTo(targetFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			return relativePath + File.separator + fileName;
-		}
-	}
+    /**
+     * 上传比赛图片文件
+     *
+     * @param file
+     * @param realPath
+     * @param rootPath
+     * @return
+     */
+    @SuppressWarnings("finally")
+    public static String uploadGamePic(MultipartFile file, String realPath) {
+        String fileName = "game_" + System.currentTimeMillis() + "."
+                + file.getOriginalFilename().split("\\.")[1];
+        String absolutePath = realPath + File.separator + UPLOAD_DIR
+                + File.separator + GAME_DIR;
+        String relativePath = File.separator + UPLOAD_DIR + File.separator
+                + GAME_DIR;
+        File targetFile = new File(absolutePath, fileName);
+        File dir = new File(absolutePath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            file.transferTo(targetFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return relativePath + File.separator + fileName;
+        }
+    }
 
-	/**
-	 * 上传球队logo
-	 * 
-	 * @param file
-	 * @param realPath
-	 * @param rootPath
-	 * @return
-	 */
-	@SuppressWarnings("finally")
-	public static String uploadTeamLogo(MultipartFile file, String realPath,
-			String name, long creatorId) {
-		String dirName = name + "_" + creatorId;
-		String fileName = "logo" + "."
-				+ file.getOriginalFilename().split("\\.")[1];
-		String relativePath = File.separator + UPLOAD_DIR + File.separator
-				+ TEAM_DIR + File.separator + dirName + File.separator + "logo";
-		String absolutePath = realPath + relativePath;
-		File dir = new File(absolutePath);
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
-		File targetFile = new File(absolutePath, fileName);
-		try {
-			file.transferTo(targetFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			return relativePath + File.separator + fileName;
-		}
-	}
-	
-	/**
-	 * 上传球队照片
-	 * @param path
-	 * @param teamDir
-	 * @param b
-	 * @return
-	 */
-	@SuppressWarnings("finally")
-	public static String uploadTeamPics(String path, String teamDir, BufferedImage bi) {
-		String fileName = System.currentTimeMillis() + ".jpg";
-		path += File.separator + "pic" + File.separator;
-		String relativePath = File.separator + UPLOAD_DIR + File.separator
-				+ TEAM_DIR + File.separator + teamDir + File.separator + "pic" + File.separator + fileName;
-		File dir = new File(path);
-		if(!dir.exists()) {
-			dir.mkdirs();
-		}
-		try {
-			ImageIO.write(bi, "jpg", new File(path + fileName)); 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			return relativePath;
-		}
-	}
+    /**
+     * 上传球队logo
+     *
+     * @param file
+     * @param realPath
+     * @param rootPath
+     * @return
+     */
+    @SuppressWarnings("finally")
+    public static String uploadTeamLogo(MultipartFile file, String realPath,
+                                        String name, long creatorId) {
+        String dirName = name + "_" + creatorId;
+        String fileName = "logo" + "."
+                + file.getOriginalFilename().split("\\.")[1];
+        String relativePath = File.separator + UPLOAD_DIR + File.separator
+                + TEAM_DIR + File.separator + dirName + File.separator + "logo";
+        String absolutePath = realPath + relativePath;
+        File dir = new File(absolutePath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File targetFile = new File(absolutePath, fileName);
+        try {
+            file.transferTo(targetFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return relativePath + File.separator + fileName;
+        }
+    }
 
-	/**
-	 * 删除某用户创建的球队的相册及头像
-	 * @param creatorId
-	 * @param realPath
-	 */
-	public static void deleteTeamAlbum(long creatorId, String contextPath) {
-		String realPath = contextPath + File.separator + UPLOAD_DIR + File.separator
-				+ TEAM_DIR;
-		File dir = new File(realPath);
-		File[] files = dir.listFiles((fileDir, name)->{
-			// TODO Auto-generated method stub
-			if(name.endsWith("_" + creatorId)) {
-				return true;
-			}else {
-				return false;
-			}
-		});
-		for(File f : files) {
-			if(f.isDirectory()) {
-				f.delete();
-			}
-		}
-	}
-	
-	/**
-	 * 删除某个文件夹下的所有文件夹和文件
-	 * 
-	 * @param delpath
-	 *            String
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 * @return boolean
-	 */
-	public static void deleteAll(String delpath) {
-		try {
-			File file = new File(delpath);
-			// 当且仅当此抽象路径名表示的文件存在且 是一个目录时，返回 true
-			if (!file.isDirectory()) {
-				file.delete();
-			} else if (file.isDirectory()) {
-				String[] filelist = file.list();
-				for (int i = 0; i < filelist.length; i++) {
-					File delfile = new File(delpath + File.separator + filelist[i]);
-					if (!delfile.isDirectory()) {
-						delfile.delete();
-					} else if (delfile.isDirectory()) {
-						deleteAll(delpath + File.separator + filelist[i]);
-					}
-				}
-				file.delete();
-			}
-		} catch (Exception e) {
-			log.error("删除目录{}错误",delpath);
-		}
-	}
+    /**
+     * 上传球队照片
+     *
+     * @param path
+     * @param teamDir
+     * @param b
+     * @return
+     */
+    @SuppressWarnings("finally")
+    public static String uploadTeamPics(String path, String teamDir, BufferedImage bi) {
+        String fileName = System.currentTimeMillis() + ".jpg";
+        path += File.separator + "pic" + File.separator;
+        String relativePath = File.separator + UPLOAD_DIR + File.separator
+                + TEAM_DIR + File.separator + teamDir + File.separator + "pic" + File.separator + fileName;
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            ImageIO.write(bi, "jpg", new File(path + fileName));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            return relativePath;
+        }
+    }
 
-	/**
-	 * 删除文件
-	 * 
-	 * @param file
-	 */
-	public static void delFile(String file) {
-		File f = new File(file);
-		if (f.exists() && f.isFile()) {
-			f.delete();
-		}
-	}
+    /**
+     * 删除某用户创建的球队的相册及头像
+     *
+     * @param creatorId
+     * @param realPath
+     */
+    public static void deleteTeamAlbum(final long creatorId, String contextPath) {
+        String realPath = contextPath + File.separator + UPLOAD_DIR + File.separator
+                + TEAM_DIR;
+        File dir = new File(realPath);
+        File[] files = dir.listFiles(new FilenameFilter() {
+                                         public boolean accept(File fileDir, String name) {
+                                             // TODO Auto-generated method stub
+                                             if (name.endsWith("_" + creatorId)) {
+                                                 return true;
+                                             } else {
+                                                 return false;
+                                             }
+                                         }
+                                     }
+        );
+        for (File f : files) {
+            if (f.isDirectory()) {
+                f.delete();
+            }
+        }
+    }
+
+    /**
+     * 删除某个文件夹下的所有文件夹和文件
+     *
+     * @param delpath String
+     * @return boolean
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public static void deleteAll(String delpath) {
+        try {
+            File file = new File(delpath);
+            // 当且仅当此抽象路径名表示的文件存在且 是一个目录时，返回 true
+            if (!file.isDirectory()) {
+                file.delete();
+            } else if (file.isDirectory()) {
+                String[] filelist = file.list();
+                for (int i = 0; i < filelist.length; i++) {
+                    File delfile = new File(delpath + File.separator + filelist[i]);
+                    if (!delfile.isDirectory()) {
+                        delfile.delete();
+                    } else if (delfile.isDirectory()) {
+                        deleteAll(delpath + File.separator + filelist[i]);
+                    }
+                }
+                file.delete();
+            }
+        } catch (Exception e) {
+            log.error("删除目录{}错误", delpath);
+        }
+    }
+
+    /**
+     * 删除文件
+     *
+     * @param file
+     */
+    public static void delFile(String file) {
+        File f = new File(file);
+        if (f.exists() && f.isFile()) {
+            f.delete();
+        }
+    }
 }
